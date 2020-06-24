7f9e86a691fbf345d5fe32ea560219b12c590427
uniQueSense <uniQueSense@users.noreply.github.com>
2020-06-24 18:16:33 +0200

    Wprowadzone zmiany - ...

--- obsluga_ruchow/poruszanie_test.py
+++ obsluga_ruchow/poruszanie_test.py
@@ -13,20 +13,17 @@ class Poruszanie_Pionka_Tests(unittest.TestCase):
     def test_poruszanie1(self):
         self.plansza[1][1] = stale.GRACZ
         self.plansza[1][2] = stale.KOMPUTER
-        wynik = poruszanie.porusz_znak(1, 1, 1, 2, stale.GRACZ, self.plansza)[0]
-        self.assertNotEqual(wynik, stale.KOMPUTER)
+        self.assertNotEqual(poruszanie.porusz_znak(1, 1, 1, 2, stale.GRACZ, self.plansza)[0], stale.KOMPUTER)
 
     def test_poruszanie2(self):
         self.plansza[0][0] = stale.GRACZ
         self.plansza[0][0] = stale.GRACZ
-        wynik = poruszanie.porusz_znak(0, 0, 0, 0, stale.GRACZ, self.plansza)[0]
-        self.assertEqual(wynik, stale.GRACZ)
+        self.assertEqual(poruszanie.porusz_znak(0, 0, 0, 0, stale.GRACZ, self.plansza)[0], stale.GRACZ)
 
     def test_poruszanie3(self):
         self.plansza[0][0] = stale.GRACZ
         self.plansza[2][2] = stale.KOMPUTER
-        wynik = poruszanie.porusz_znak(0, 0, 2, 2, stale.KOMPUTER, self.plansza)[0]
-        self.assertEqual(wynik, stale.KOMPUTER)
+        self.assertEqual(poruszanie.porusz_znak(0, 0, 2, 2, stale.KOMPUTER, self.plansza)[0], stale.KOMPUTER)
 
 
 
